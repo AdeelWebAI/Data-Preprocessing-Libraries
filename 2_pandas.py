e15b854048f6c2e7d31385aff62bbf0953af3ae2
adeel <adeelwaraich27@gmail.com>
2025-08-13 01:19:14 +0500

    grouping, concatenation , merging

--- 2_pandas.py
+++ 2_pandas.py
@@ -93,11 +93,51 @@ import pandas as pd
 
 # a. Grouping Data
 
-# print(df.groupby("Random"))    # Group by Random column 
+df=pd.read_excel("E_post2.xlsx")
 
+# print(df.groupby("Random"))    # Group by Random column 
+# to see the group we run some function on the group 
+
+# print(df.groupby("Random").sum())  # this will print the groups with function 
+
+# to see the group we run loop 
+# for name, group in df.groupby("Random"):
+#     print(f"Group: {name}")
+#     print(group)
+#     print()
+    
+    
 # print(df.groupby("Random")["Random2"].sum()) 
 
 
+# b. Aggregation Functions
 
-data=pd.read_excel("E_post2.xlsx")
-print(data)
+# used to perform multiple operations of of a group or data frame
+
+# print(df.agg({"Name":"sum","District":"max"}))
+
+
+# print(df.groupby("Random").agg({"Name":["size","max"]})) 
+
+
+# 7. Merging and Joining Data
+
+# a. Concatenation
+
+# df1 = pd.DataFrame({"id":[1,2,3,4],"name":["A","B","C","D"]})
+# df2 = pd.DataFrame({"id":[5,6,7,8],"name":["E","F","G","H"]})
+
+# result = pd.concat([df1, df2], ignore_index=True)
+
+# print(result)
+
+
+# b. Merging DataFrames
+
+# df1 = pd.DataFrame({"id":[1, 2, 3],"Salary":[1000,2000,3000]})
+# df2 = pd.DataFrame({"id":[1, 2, 4],"Salary":[2000,3000,4000]})
+# merged_df = merged_df = df1.merge(df2, on="id" ,  how="inner") # merge common values
+# merged_df = merged_df = df1.merge(df2, on="id" ,  how="outer") # merge both values either common or not
+# print(merged_df)
+
+
